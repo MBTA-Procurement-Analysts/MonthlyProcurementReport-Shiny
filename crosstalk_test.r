90c34cad6bfd35aaf4a06b89ef0eced707fccb68
Guo <nguo@MBTA.COM>
2018-07-13 17:28:07 -0400

    Added crosstalk test (WIP), imported bootstrap for styling, tidied main app

--- crosstalk_test.r
+++ crosstalk_test.r
@@ -1,5 +1,5 @@
-# MonthlyProcurementReport-Shiny/app.r
-# Recreating a MPR using Plotly and Shiny
+# MonthlyProcurementReport-Shiny/crosstalk_test.r
+# Project-specific sandbox for Crosstalk
 # By: Nianmin Guo
 
 # Setup -------------------------------------------------------------------
@@ -14,32 +14,16 @@ library(shiny)
 library(crosstalk)
 library(magrittr)
 
+setwd("C:/Users/nguo/Documents/github/MonthlyProcurementReport-Shiny")
 
 # Plotly APIs
 Sys.setenv("plotly_username" = "Zenmai0822")
 Sys.setenv("plotly_api_key" = "1qC2QkZBYFrJzOG9RW9i")
 
-
-# VARIABLES  -- VERIFY BEFORE RUNNING -------------------------------------
-
-# Working Directory
-setwd("C:/Users/nguo/Documents/github/MonthlyProcurementReport-Shiny")
-
-# Two previous months, in chronological order
-prevmos <- c("May", "Jun")
-
-
-# Constants, Functions ----------------------------------------------------
-
-# Dollar Formatting for Tables, not used now since DT can format itself
+# Dollar Formatting for Tables
 usd <- dollar_format(largest_with_cents = 1e+15, prefix = "$")
 
-
-# FY Month Factors, July first
-fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec", 
-                "Jan", "Feb", "Mar", "Apr", "May", "Jun")
-
-# Data Import -------------------------------------------------------------
+# Data Inport -------------------------------------------------------------
 
 (raw_po <- readxl::read_excel("plotly-report.xlsx", 
                               sheet = "sheet1", 
@@ -68,6 +52,11 @@ fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
 
 
 # Raw Data Handling -------------------------------------------------------
+
+# FY Month Factors, July first
+fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec", 
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun")
+
 
 # PER LINE ITEMS
 
@@ -146,170 +135,27 @@ fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
               FifthPct = dplyr::nth(`Sum Amount`, round(0.05 * n()))))
 
 
-# Plotting: Using ---------------------------------------------------------
-
-plot_line_month_98perc <- monthly_98perc %>% 
-  plot_ly(x = ~Month, 
-          y = ~`Monthly_Sum`, 
-          type = "scatter", 
-          mode = "lines",
-          hoveron = "points", 
-          marker = list(symbol = 200,
-                        size = 8)) %>% 
-  layout(xaxis = list(title = "FY '18 Month"),
-         yaxis = list(title = "Monthly Sum"))
-
-plot_line_month_all <- monthly_all %>% 
-  plot_ly(x = ~`Month`, 
-          y = ~`Monthly_Sum`, 
-          type = "scatter", 
-          mode = "lines",
-          hoveron = "points", 
-          marker = list(symbol = 200,
-                        size = 8)) %>% 
-  layout(xaxis = list(title = "FY '18 Month"),
-         yaxis = list(title = "Monthly Sum"))
-
-plot_pie_spend_bunit <- po_spend_bunitfct %>% 
-  plot_ly(x = 0, 
-          y = ~Perc, 
-          color = ~Unit, 
-          type = "bar") %>% 
-  add_text(x = 0, 
-           y = ~CumSum, 
-           text = ~Unit, 
-           textfont = list(color = c("#000000"), 
-                           size = 12)) %>% # make text labels go to half of current y
-  layout(xaxis = list(title = "Business Unit",
-                      showticklabels = FALSE, 
-                      range = list(-0.5, 0.5)),
-         yaxis = list(title = "", 
-                      tickformat = "%"), 
-         barmode = 'stack', 
-         bargap = 0.45, 
-         showlegend = FALSE)
-
-# Plotting: Unused --------------------------------------------------------
-
-plot_box_month_all <- raw_po %>% 
-  plot_ly(x = ~`Month`, 
-          y = ~`Sum Amount`, 
-          type = "box")
-
-plot_box_month_98perc <- po_98perc %>% 
-  plot_ly(x = ~Month, 
-          y = ~`Sum Amount`, 
-          type = "box")
-
-plot_monthly_po_spend <- plot_ly(monthly_po_spend) %>% 
-  add_lines(x = ~Month, y = ~Monthly_Sum)
-
-plot_monthly_po_count <- plot_ly(monthly_po_spend) %>% 
-  add_lines(x = ~Month, y = ~Monthly_Count)
-
-# Median Line of POs by month, with shading of 95% and 5% regions by month
-# Pending
-plot_line_mean_month_98perc <- po_month_98perc_9505 %>% 
-  plot_ly(x = ~Month, 
-          y = ~NinetyFifthPct,
-          line = list(color = 'transparent'), 
-          name = "95th Percentile", type = 'scatter', mode = 'lines', showlegend = FALSE) %>% 
-  add_trace(x = ~Month,
-            y = ~FifthPct,
-            fill = 'tonexty',
-            line = list(color = ' transparent'),
-            fillcolor = 'rgba(0,0,255,0.2)',
-            name = "5th Percentile") %>% 
-  add_trace(x = ~Month,
-            y = ~Median, 
-            line = list(color = 'rgb(0,0,255)'), 
-            name = "Median")
-
-# Data Tables -------------------------------------------------------------
-
-(po_98perc_top10_table <- po_98perc_top10 %>% 
-   mutate_at("PO Date", date))
-
-(po_all_top10_table <- po_all_top10 %>% 
-    mutate_at("PO Date", date))
-
-# Hack, not a tidy table, don't learn from this
-
-(po_2moago_bunit_table <- raw_po %>% 
-    filter(Month == prevmos[1]) %>% 
-    group_by(Month, Unit) %>% 
-    summarise(Count = n(), Sum = sum(`Sum Amount`)))
-
-(po_1moago_bunit_table <- raw_po %>% 
-    filter(Month == prevmos[2]) %>% 
-    group_by(Month, Unit) %>% 
-    summarise(Count = n(), Sum = sum(`Sum Amount`)))
-
-(po_prev2mos_bunit_table <- raw_po %>% 
-    filter(Month %in% prevmos) %>% 
-    group_by(Month) %>% 
-    summarise(Count = n(), Sum = sum(`Sum Amount`)) %>% 
-    mutate(Unit = paste(as.character(Month), "Total")) %>% 
-    select(Month, Unit, everything()))
-
-(po_prev2mos_gt_table <- po_prev2mos_bunit_table %>% 
-    summarise(Month = NA, Unit = "Grand Total", Count = sum(Count), Sum = sum(Sum)))
-
-(po_prev2mos_all_table <- bind_rows(po_2moago_bunit_table, 
-                                    po_prev2mos_bunit_table[1,], 
-                                    po_1moago_bunit_table, 
-                                    po_prev2mos_bunit_table[2,]) %>% 
-    ungroup(Month) %>% 
-    select(-Month))
-
-(po_spend_bunit_table <- po_spend_bunit %>% 
-    select(Unit, Count, Sum))
-
-(po_spend_gt_table <- po_spend_bunit_table %>% 
-    summarise(Unit = "Grand Total",  Count = sum(Count), Sum = sum(Sum)))
-
-
-(po_spend_bunit_all <- bind_rows(po_spend_bunit_table,
-                                 po_spend_gt_table))
-
 # Shiny -------------------------------------------------------------------
 
-ui <- fluidPage(responsive = TRUE, 
-                #theme = "bootstrap.css",
+ui <- fluidPage(
   verticalLayout(
-    fluidRow(column(8, 
-                    h1("Overall PO Count and Spend")),
-             column(2, offset = 2, 
-                    img(src = "t-logo.jpg", 
-                        style = "float:right",
-                        width = "75px", 
-                        height = "75px",
-                        class = "p-1"))),
-    tags$hr(),
-    fluidRow(column(8, 
-                    h2("All POs FY 2018"),
-                    p("With 12,378 POs"),
-                    plotlyOutput("p1_line_all"),
-                    DTOutput("d1_all")),
-             
-             column(4,
-                    h2("POs FY 2018"),
-                    p("With 248 outliers (2%) removed"),
-                    plotlyOutput("p2_line_98pc"),
-                    DTOutput("d2_98pc"))),
-    tags$hr(),
-    fluidRow(column(6, 
-                    h3("POs of Previous 2 Months"),
-                    DTOutput("d3_prev2mo")),
-             column(6,
-                    fluidRow(
-                      h3("FY18 Total Spend by Business Unit"), 
-                      column(4, align = "center", plotlyOutput("p3_pie_spend_bunit")),
-                      column(8, DTOutput("d4_ytd_bunit"))))),
-    tags$hr(),
-    p(style = "text-align:right", "Draft for Discussion and Policy Purposes Only")))
+    plotlyOutput("p1"),
+    DTOutput("d1")
+  )
+)
 
 server <- function(input, output) {
+  
+  share_raw_po <- ShareData$new(raw_po)
+  
+  selected <- input$d1_rows_selected
+  
+  if (!length(selected)) {
+    share_raw_po %>% 
+      group_by(Month) %>% 
+      summarise(summarise(Monthly_Sum = sum(`Sum Amount`)))
+    # NOT DONE YET
+  }
   
   output$p1_line_all <- renderPlotly(plot_line_month_all)
   output$d1_all <- renderDT(DT::datatable(po_all_top10_table, 
@@ -317,8 +163,7 @@ server <- function(input, output) {
                                           options = list(dom = "t",
                                                          columnDefs = list(list(className = 'dt-left', targets = 0:1),
                                                                            list(className = 'dt-right', targets = 2))),
-                                          caption = "Top 10 POs from all") %>% 
-                              formatCurrency(c("Sum Amount")))
+                                          caption = "Top 10 POs from all"))
   
   output$p2_line_98pc <- renderPlotly(plot_line_month_98perc)
   output$d2_98pc <- renderDT(DT::datatable(po_98perc_top10_table, 
@@ -326,8 +171,7 @@ server <- function(input, output) {
                                            options = list(dom = "t",
                                                           columnDefs = list(list(className = 'dt-left', targets = 0:1),
                                                                             list(className = 'dt-right', targets = 2))),
-                                           caption = "Top 10 POs from 98% percentile.") %>% 
-                               formatCurrency(c("Sum Amount")))
+                                           caption = "Top 10 POs from 98% percentile."))
   
   output$d3_prev2mo <- renderDT(DT::datatable(po_prev2mos_all_table,
                                               rownames = FALSE, 
@@ -351,5 +195,3 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
-
-# Scratchpad --------------------------------------------------------------

fe582a174cf520ef79dfd36c95631f7915f15065
Guo <nguo@MBTA.COM>
2018-07-16 17:20:34 -0400

    Added median shades for monthly sum, scale to be determined.

--- app.r
+++ app.r
@@ -62,21 +62,21 @@ fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
                                             "text")))
 
 # from Pivot table, only a groupby month and unit summary
-(pivot_po <- readxl::read_excel("plotly-report.xlsx", sheet = "Monthly PO Breakdown"))
+# (pivot_po <- readxl::read_excel("plotly-report.xlsx", sheet = "Monthly PO Breakdown"))
 
 # Pivot Table Data Handling -----------------------------------------------
 
-(pivot_po %<>% 
-   slice(1:(n()-1)) %>% # Remove last line (G. Total)
-   mutate(Month = rep(month.abb, each = 5)) %>% # create new variable that is (Inflate a Month list 5 times each)
-   rename(`Biz Unit` = `Row Labels`, `Sum` = `Sum of Sum Amount`, `Count` = `Distinct Count of PO No.` ) %>%  # Rename Cols
-   select(Month, everything()) %>% # Rearrange columns
-   filter(!Month == `Biz Unit`) %>% # Filter cases where Biz Unit is the month, gets rids of monthly totals
-   mutate_at(c("Month", "Biz Unit"), as_factor)) # mutate to factors for Month and BUnit
-
-(monthly_po_spend <- pivot_po %>% 
-    group_by(Month) %>% 
-    summarise(Monthly_Sum = sum(`Sum`), Monthly_Count = sum(`Count`))) 
+# (pivot_po %<>% 
+#    slice(1:(n()-1)) %>% # Remove last line (G. Total)
+#    mutate(Month = rep(month.abb, each = 5)) %>% # create new variable that is (Inflate a Month list 5 times each)
+#    rename(`Biz Unit` = `Row Labels`, `Sum` = `Sum of Sum Amount`, `Count` = `Distinct Count of PO No.` ) %>%  # Rename Cols
+#    select(Month, everything()) %>% # Rearrange columns
+#    filter(!Month == `Biz Unit`) %>% # Filter cases where Biz Unit is the month, gets rids of monthly totals
+#    mutate_at(c("Month", "Biz Unit"), as_factor)) # mutate to factors for Month and BUnit
+# 
+# (monthly_po_spend <- pivot_po %>% 
+#     group_by(Month) %>% 
+#     summarise(Monthly_Sum = sum(`Sum`), Monthly_Count = sum(`Count`))) 
 
 
 # Raw Data Handling -------------------------------------------------------
@@ -84,13 +84,13 @@ fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
 # PER LINE ITEMS
 
 (raw_req <- raw_po %>% 
-    mutate(Month = month(`PO Date`, label = TRUE, abbr = TRUE)) %>% # Month for PO, in factor
-    mutate_at("Month", ~parse_factor(., levels = fy_factors)) %>% 
-    select(-Line) %>% 
-    group_by(`PO No.`) %>% 
-    mutate(`Sum` = sum(`Sum Amount`)) %>% 
-    select(Sum, everything()) %>%
-    select(Month, everything())) # bump Month to first 
+   mutate(Month = month(`PO Date`, label = TRUE, abbr = TRUE)) %>% # Month for PO, in factor
+   mutate_at("Month", ~parse_factor(., levels = fy_factors)) %>% 
+   select(-Line) %>% 
+   group_by(`PO No.`) %>% 
+   mutate(`Sum` = sum(`Sum Amount`)) %>% 
+   select(Sum, everything()) %>%
+   select(Month, everything())) # bump Month to first 
 
 # PER PO, DISTINCT VARS BY LINE ITEM HAS BEEN REMOVED
 
@@ -157,10 +157,18 @@ fy_factors <- c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
               NinetyFifthPct = dplyr::nth(`Sum Amount`, round(0.95 * n())),
               FifthPct = dplyr::nth(`Sum Amount`, round(0.05 * n()))))
 
+(po_month_98perc_median <- po_98perc %>% 
+    group_by(Month) %>% 
+    summarise(`Monthly_Sum` = sum(`Sum Amount`)) %>% 
+    summarise(`Overall_Median` = median(`Monthly_Sum`), `Overall_SD` = sd(`Monthly_Sum`)) %>% 
+    mutate(`Upper_Limit` = `Overall_Median` + 1 * `Overall_SD`, 
+           `Lower_Limit` = `Overall_Median` - 1 * `Overall_SD`,
+           `Upper_Limit2x` = `Overall_Median` + 2 * `Overall_SD`, 
+           `Lower_Limit2x` = `Overall_Median` - 2 * `Overall_SD`))
 
 # Plotting: Using ---------------------------------------------------------
 
-plot_line_month_98perc <- monthly_98perc %>% 
+(plot_line_month_98perc <- monthly_98perc %>% 
   plot_ly(x = ~Month, 
           y = ~`Monthly_Sum`, 
           type = "scatter", 
@@ -169,7 +177,24 @@ plot_line_month_98perc <- monthly_98perc %>%
           marker = list(symbol = 200,
                         size = 8)) %>% 
   layout(xaxis = list(title = "FY '18 Month"),
-         yaxis = list(title = "Monthly Sum"))
+         yaxis = list(title = "Monthly Sum"),
+         shapes = list(
+           list(type = "rect", 
+           fillcolor = "blue",
+           line = list(color = "blue"), 
+           opacity = 0.2, 
+           x0 = 'Jul', x1 = 'Jun', xref = "x",
+           y0 = po_month_98perc_median$Lower_Limit,
+           y1 = po_month_98perc_median$Upper_Limit,
+           yref = "y"), 
+           list(type = "rect", 
+           fillcolor = "blue",
+           line = list(color = "blue"), 
+           opacity = 0.2, 
+           x0 = 'Jul', x1 = 'Jun', xref = "x",
+           y0 = po_month_98perc_median$Lower_Limit2x,
+           y1 = po_month_98perc_median$Upper_Limit2x,
+           yref = "y"))))
 
 plot_line_month_all <- monthly_all %>% 
   plot_ly(x = ~`Month`, 
@@ -203,39 +228,39 @@ plot_pie_spend_bunit <- po_spend_bunitfct %>%
 
 # Plotting: Unused --------------------------------------------------------
 
-plot_box_month_all <- raw_po %>% 
-  plot_ly(x = ~`Month`, 
-          y = ~`Sum Amount`, 
-          type = "box")
-
-plot_box_month_98perc <- po_98perc %>% 
-  plot_ly(x = ~Month, 
-          y = ~`Sum Amount`, 
-          type = "box")
-
-plot_monthly_po_spend <- plot_ly(monthly_po_spend) %>% 
-  add_lines(x = ~Month, y = ~Monthly_Sum)
-
-plot_monthly_po_count <- plot_ly(monthly_po_spend) %>% 
-  add_lines(x = ~Month, y = ~Monthly_Count)
-
-# Median Line of POs by month, with shading of 95% and 5% regions by month
-# Pending
-plot_line_mean_month_98perc <- po_month_98perc_9505 %>% 
-  plot_ly(x = ~Month, 
-          y = ~NinetyFifthPct,
-          line = list(color = 'transparent'), 
-          name = "95th Percentile", type = 'scatter', mode = 'lines', showlegend = FALSE) %>% 
-  add_trace(x = ~Month,
-            y = ~FifthPct,
-            fill = 'tonexty',
-            line = list(color = ' transparent'),
-            fillcolor = 'rgba(0,0,255,0.2)',
-            name = "5th Percentile") %>% 
-  add_trace(x = ~Month,
-            y = ~Median, 
-            line = list(color = 'rgb(0,0,255)'), 
-            name = "Median")
+# plot_box_month_all <- raw_po %>% 
+#   plot_ly(x = ~`Month`, 
+#           y = ~`Sum Amount`, 
+#           type = "box")
+# 
+# plot_box_month_98perc <- po_98perc %>% 
+#   plot_ly(x = ~Month, 
+#           y = ~`Sum Amount`, 
+#           type = "box")
+# 
+# plot_monthly_po_spend <- plot_ly(monthly_po_spend) %>% 
+#   add_lines(x = ~Month, y = ~Monthly_Sum)
+# 
+# plot_monthly_po_count <- plot_ly(monthly_po_spend) %>% 
+#   add_lines(x = ~Month, y = ~Monthly_Count)
+# 
+# # Median Line of POs by month, with shading of 95% and 5% regions by month
+# # Pending
+# plot_line_mean_month_98perc <- po_month_98perc_9505 %>% 
+#   plot_ly(x = ~Month, 
+#           y = ~NinetyFifthPct,
+#           line = list(color = 'transparent'), 
+#           name = "95th Percentile", type = 'scatter', mode = 'lines', showlegend = FALSE) %>% 
+#   add_trace(x = ~Month,
+#             y = ~FifthPct,
+#             fill = 'tonexty',
+#             line = list(color = ' transparent'),
+#             fillcolor = 'rgba(0,0,255,0.2)',
+#             name = "5th Percentile") %>% 
+#   add_trace(x = ~Month,
+#             y = ~Median, 
+#             line = list(color = 'rgb(0,0,255)'), 
+#             name = "Median")
 
 # Data Tables -------------------------------------------------------------
 
@@ -286,38 +311,38 @@ plot_line_mean_month_98perc <- po_month_98perc_9505 %>%
 
 ui <- fluidPage(responsive = TRUE, 
                 #theme = "bootstrap.css",
-  verticalLayout(
-    fluidRow(column(8, 
-                    h1("Overall PO Count and Spend")),
-             column(2, offset = 2, 
-                    img(src = "t-logo.jpg", 
-                        style = "float:right",
-                        width = "75px", 
-                        height = "75px",
-                        class = "p-1"))),
-    tags$hr(),
-    fluidRow(column(8, 
-                    h2("All POs FY 2018"),
-                    p("With 12,378 POs"),
-                    plotlyOutput("p1_line_all"),
-                    DTOutput("d1_all")),
-             
-             column(4,
-                    h2("POs FY 2018"),
-                    p("With 248 outliers (2%) removed"),
-                    plotlyOutput("p2_line_98pc"),
-                    DTOutput("d2_98pc"))),
-    tags$hr(),
-    fluidRow(column(6, 
-                    h3("POs of Previous 2 Months"),
-                    DTOutput("d3_prev2mo")),
-             column(6,
-                    fluidRow(
-                      h3("FY18 Total Spend by Business Unit"), 
-                      column(4, align = "center", plotlyOutput("p3_pie_spend_bunit")),
-                      column(8, DTOutput("d4_ytd_bunit"))))),
-    tags$hr(),
-    p(style = "text-align:right", "Draft for Discussion and Policy Purposes Only")))
+                verticalLayout(
+                  fluidRow(column(8, 
+                                  h1("Overall PO Count and Spend")),
+                           column(2, offset = 2, 
+                                  img(src = "t-logo.jpg", 
+                                      style = "float:right",
+                                      width = "75px", 
+                                      height = "75px",
+                                      class = "p-1"))),
+                  tags$hr(),
+                  fluidRow(column(8, 
+                                  h2("All POs FY 2018"),
+                                  p("With 12,377 POs"),
+                                  plotlyOutput("p1_line_all"),
+                                  DTOutput("d1_all")),
+                           
+                           column(4,
+                                  h2("POs FY 2018"),
+                                  p("With 248 outliers (2%) removed. Shaded area represents normal range (1SD)."),
+                                  plotlyOutput("p2_line_98pc"),
+                                  DTOutput("d2_98pc"))),
+                  tags$hr(),
+                  fluidRow(column(6, 
+                                  h3("POs of Previous 2 Months"),
+                                  DTOutput("d3_prev2mo")),
+                           column(6,
+                                  fluidRow(
+                                    h3("FY18 Total Spend by Business Unit"), 
+                                    column(4, align = "center", plotlyOutput("p3_pie_spend_bunit")),
+                                    column(8, DTOutput("d4_ytd_bunit"))))),
+                  tags$hr(),
+                  p(style = "text-align:right", "Draft for Discussion and Policy Purposes Only")))
 
 server <- function(input, output) {
   

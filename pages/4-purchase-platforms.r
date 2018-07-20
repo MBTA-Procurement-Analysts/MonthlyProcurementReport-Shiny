d3c008156c1fcf2fd6e7042e41dfa8df1ee449ce
Nianmin Guo <nguo@MBTA.com>
2018-07-20 16:46:37 -0400

    Modified Pg4 Threshold Table, added bins, imported platform csv

--- pages/4-purchase-platforms.r
+++ pages/4-purchase-platforms.r
@@ -21,7 +21,9 @@ prevmos <- c("May", "Jun")
 
 # Spending Thresholds (Buckets)
 # Per discussion with Scott on 7/19/2018 Thursday Afternoon
-threshold_factors <- c("<1,000", "1,000-3,500", "3,500-5,000", "5,000-50,000", ">50,000")
+threshold_5_factors <- c("<1,000", "1,000-3,500", "3,500-5,000", "5,000-50,000", ">50,000")
+
+threshold_3_factors <- c("Micro", "Small", "Large")
 
 # Data Import -------------------------------------------------------------
 # This page shares some of the data used by page 2 (Overall spending)
@@ -40,6 +42,14 @@ threshold_factors <- c("<1,000", "1,000-3,500", "3,500-5,000", "5,000-50,000", "
                                             "text", "text", "text", "text", "text", 
                                             "numeric", "numeric", "text", "text", "text", 
                                             "text")))
+
+# Raw data with platform information, from Nate on 7/19/18. 
+# Discussed entries with multiple Platforms marked with Nate on 7/20/18,
+# Made modifications to the source csv file.
+
+(raw_platform_po <- as_tibble(read.csv("data/platformdata.csv")) %>% 
+    replace_na(FairMarkIt. = FALSE, FMIS = FALSE) %>% 
+    mutate(FairMarkIt. = as.logical(FairMarkIt.), FMIS = as.logical(FMIS)))
 
 
 # Raw Data Handling -------------------------------------------------------
@@ -68,28 +78,63 @@ threshold_factors <- c("<1,000", "1,000-3,500", "3,500-5,000", "5,000-50,000", "
     dplyr::distinct() %>%
     rename(`Sum Amount` = Sum))
 
+(pg4_platform_po <- raw_platform_po %>% 
+    mutate_at("PO.Date", as_date) %>% 
+    mutate(Month = month(`PO.Date`, label = TRUE, abbr = TRUE)) %>% # Month for PO, in factor
+    mutate_at("Month", ~parse_factor(., levels = fy_factors)) %>%
+    select(-Line) %>%
+    group_by(`PO.No.`) %>%
+    mutate(Sum = sum(`Sum.Amount`), Sum_Qty = sum(`PO.Qty`)) %>%
+    select(Month, everything(), -`PO.Qty`, -`Sum.Amount`, -`Mfg.Itm.ID`, -`Mfg.ID`, -`Level.1`, -`Level.2`) %>%
+    ungroup(`PO.No.`) %>%
+    dplyr::distinct() %>%
+    rename(`Sum Amount` = Sum))
+
 # Added Threshold Categorization
 # L: >= [50'000, up]; M: (3'500, 50'000); S: [0, 3'500]
 
 (pg4_threshold_po <- pg4_raw_po %>% 
     mutate(Threshold = case_when(
-      `Sum Amount` >= 50000 ~ parse_factor(">50,000", levels = threshold_factors),
-      `Sum Amount` <= 50000 & `Sum Amount` > 5000 ~ parse_factor("5,000-50,000", levels = threshold_factors), 
-      `Sum Amount` <= 5000 & `Sum Amount` > 3500 ~ parse_factor("3,500-5,000", levels = threshold_factors), 
-      `Sum Amount` <= 3500 & `Sum Amount` > 1000 ~ parse_factor("1,000-3,500", levels = threshold_factors), 
-      `Sum Amount` <= 1000 ~ parse_factor("<1,000", levels = threshold_factors))))
+      `Sum Amount` >= 50000 ~ parse_factor(">50,000", levels = threshold_5_factors),
+      `Sum Amount` <= 50000 & `Sum Amount` > 5000 ~ parse_factor("5,000-50,000", levels = threshold_5_factors), 
+      `Sum Amount` <= 5000 & `Sum Amount` > 3500 ~ parse_factor("3,500-5,000", levels = threshold_5_factors), 
+      `Sum Amount` <= 3500 & `Sum Amount` > 1000 ~ parse_factor("1,000-3,500", levels = threshold_5_factors), 
+      `Sum Amount` <= 1000 ~ parse_factor("<1,000", levels = threshold_5_factors))) %>% 
+    mutate(Category = case_when(
+      `Threshold` == ">50,000" ~ parse_factor("Large", levels = threshold_3_factors),
+      `Threshold` %in% c("5,000-50,000", "3,500-5,000") ~ parse_factor("Small", levels = threshold_3_factors),
+      `Threshold` %in% c("1,000-3,500", "<1,000") ~ parse_factor("Micro", levels = threshold_3_factors))))
 
-(pg4_threshold_sum_cnt <- pg4_threshold_po %>% 
+# A version of summation by 5 thresholds only, for plotting purposes
+
+(pg4_threshold_sum_cnt_nocat <- pg4_threshold_po %>% 
     group_by(Threshold) %>% 
     summarise(Count = n(), Spend = sum(`Sum Amount`)) %>% 
     mutate(PercCnt = Count / sum(Count), PercSum = Spend / sum(Spend)) %>% 
     select(Threshold, Count, PercCnt, Spend, PercSum))
 
+# A version of summation by 3 and 5 thresholds, by doing this the each 5 
+#   threshold line has a percentage of its 3 threshold parent, instead of
+#   the whole sum amount.
+
+(pg4_threshold_sum_cnt <- pg4_threshold_po %>% 
+    group_by(Category, Threshold) %>% 
+    summarise(Count = n(), Spend = sum(`Sum Amount`)) %>% 
+    mutate(PercCnt = Count / sum(Count), PercSum = Spend / sum(Spend)) %>% 
+    select(Category, Threshold, Count, PercCnt, Spend, PercSum))
+
 (pg4_sub1k_bunit <- pg4_threshold_po %>% 
     filter(`Threshold` == '<1,000') %>% 
     group_by(Unit) %>% 
     summarise(Count = n()) %>% 
-    mutate(PectCount = Count / sum(Count)))
+    mutate(PercCnt = Count / sum(Count)) %>% 
+    arrange(desc(PercCnt)))
+
+pg4_sub1k_bunit_fct <- pg4_sub1k_bunit %>% pull(Unit)
+
+(pg4_sub1k_bunit_plot_tbl <- pg4_sub1k_bunit %>% 
+    mutate_at("Unit", ~parse_factor(., levels = pg4_sub1k_bunit_fct)) %>% 
+    mutate(PlotY = cumsum(PercCnt) - 0.5 * PercCnt))
 
 (pg4_sub1k_bunit_sum <- pg4_threshold_po %>% 
     filter(`Threshold` == '<1,000') %>% 
@@ -97,7 +142,8 @@ threshold_factors <- c("<1,000", "1,000-3,500", "3,500-5,000", "5,000-50,000", "
 
 # Plots, Using ------------------------------------------------------------
 
-pg4_plot_thresholds <- plot_ly(data = pg4_threshold_sum_cnt, x = ~Threshold) %>% 
+pg4_plot_thresholds <- pg4_threshold_sum_cnt_nocat %>% 
+  plot_ly(x = ~Threshold) %>% 
   add_trace(y = ~PercSum, 
             type = 'bar', 
             text = ~paste(currency(Spend / 1000000, "$", digits = 1), "M"), 
@@ -121,11 +167,11 @@ pg4_plot_thresholds <- plot_ly(data = pg4_threshold_sum_cnt, x = ~Threshold) %>%
 
 # layout(yaxis = list(tickprefix = "$"))
 
-pg4_plot_sum <- plot_ly(data = pg4_threshold_sum_cnt, 
+pg4_plot_sum <- plot_ly(data = pg4_threshold_sum_cnt_nocat, 
                         x = ~Threshold, 
                         y = ~Spend, 
                         type = "bar",
-                        text = ~paste(percent(PercSum, 2)),
+                        text = ~paste(percent(PercSum, 1)),
                         textposition = 'auto',
                         name = "Sum of Spendings",
                         showlegend = FALSE, 
@@ -134,11 +180,11 @@ pg4_plot_sum <- plot_ly(data = pg4_threshold_sum_cnt,
          yaxis = list(title = "Sum of Spendings",
                       tickprefix = "$"))
 
-pg4_plot_cnt <- plot_ly(data = pg4_threshold_sum_cnt, 
+pg4_plot_cnt <- plot_ly(data = pg4_threshold_sum_cnt_nocat, 
                         x = ~Threshold, 
                         y = ~Count, 
                         type = "bar",
-                        text = ~paste(percent(PercCnt, 2)),
+                        text = ~paste(percent(PercCnt, 1)),
                         textposition = 'auto',
                         name = 'Count of POs', 
                         showlegend = FALSE,
@@ -152,19 +198,56 @@ pg4_plot_threshold_sum_cnt <- subplot(pg4_plot_sum,
                                       shareX = TRUE,
                                       titleY = TRUE)
 
+(pg4_plot_sub1k_count_bunit <- pg4_sub1k_bunit_plot_tbl %>% 
+    plot_ly(x = 0,
+            y = ~PercCnt,
+            color = ~Unit, 
+            type = 'bar') %>% 
+    add_text(x = c(0,0,-0.1,0.1),
+             y = ~PlotY, 
+             text = ~paste(Unit, ":", Count), 
+             textfont = list(color = c("#000000"))) %>% 
+    layout(xaxis = list(title = "Business Unit",   
+                        showticklabels = FALSE,    
+                        range = list(-0.5, 0.5)),  
+           yaxis = list(title = "PO Count",        
+                        tickformat = "%"),         
+           barmode = 'stack',                      
+           bargap = 0.55,                          
+           showlegend = FALSE))
+
 
 
 # Data Tables, and related data wrangling ---------------------------------
 
-pg4_threshold_gt_table <- pg4_threshold_sum_cnt %>% 
-  summarise(Threshold = 'Grand Total', Count = sum(Count), PercCnt = sum(PercCnt), Spend = sum(Spend), PercSum = sum(PercSum))
+(pg4_threshold_gt_table <- pg4_threshold_sum_cnt_nocat %>% 
+   ungroup(Category) %>%
+   summarise(Category = '', 
+             Threshold = 'Grand Total', 
+             Count = sum(Count), 
+             PercCnt = sum(PercCnt), 
+             Spend = sum(Spend), 
+             PercSum = sum(PercSum)))
 
-pg4_threshold_table <- bind_rows(pg4_threshold_sum_cnt, pg4_threshold_gt_table) %>% 
-  dplyr::rename(`FY18 Thresholds` = `Threshold`, 
-                `Count of POs` = `Count`, 
-                `Percent of PO Count` = `PercCnt`, 
-                `FY18 Spend` = `Spend`, 
-                `Percent of FY18 Spend` = `PercSum`)
+(pg4_threshold_totals_table <- pg4_threshold_sum_cnt %>% 
+    summarise(Threshold = paste(`Category`, "Subtotal")[1], 
+              Count = sum(Count), 
+              PercCnt = Count / pg4_threshold_gt_table$Count, 
+              Spend = sum(Spend), 
+              PercSum = Spend / pg4_threshold_gt_table$Spend))
+
+# Desired Modification: Blank out Subtotals at Categories, but it seems like
+#   that str_detect does not take colnames. Skipping for now.
+
+(pg4_threshold_table <- bind_rows(pg4_threshold_sum_cnt, pg4_threshold_totals_table) %>% 
+    arrange(Category) %>% 
+    bind_rows(pg4_threshold_gt_table) %>% 
+    # mutate_if(~str_detect(`Threshold`, "Total"), Category = "") %>% 
+    dplyr::rename(`FY18 Thresholds` = `Threshold`, 
+                  `Count of POs` = `Count`, 
+                  `Percent of PO Count` = `PercCnt`, 
+                  `FY18 Spend` = `Spend`, 
+                  `Percent of FY18 Spend` = `PercSum`))
 
 (pg4_2moago_threshold_table <- pg4_threshold_po %>% 
     filter(Month == prevmos[1]) %>% 
@@ -207,15 +290,22 @@ uipg4 <- tabPanel("Spend/Count by Platforms",
                     tags$hr(),
                     h3("FY18 Purchasing by Threshold"),
                     fluidRow(
-                      column(4, verticalLayout(
+                      column(5, verticalLayout(
                         DT::dataTableOutput("pg4_threshold_sum_cnt_dt"),
                         tags$hr(),
                         h3("Last 2 months of Purchasing by Threshold"),
                         DT::dataTableOutput("pg4_2moago_threshold"),
                         DT::dataTableOutput("pg4_1moago_threshold"))), 
-                      column(4,
-                             plotlyOutput("pg4_plot_thresholds", height = "600px")),
-                      column(4,
-                             plotlyOutput("pg4_plot_threshold_sum_cnt", height = "600px"))),
-                    tags$hr(),
-                    p(style = "text-align:right", "Draft for Discussion and Policy Purposes Only")))
+                      column(7, 
+                             fluidRow(
+                               column(7,
+                                      plotlyOutput("pg4_plot_thresholds", height = "600px")),
+                               column(5,
+                                      plotlyOutput("pg4_plot_threshold_sum_cnt", height = "600px"))), 
+                             tags$hr(),
+                             fluidRow(
+                               column(6,
+                                      plotlyOutput("pg4_plot_sub100_bunit_count")))),
+                      tags$hr(),
+                      p(style = "text-align:right", "Draft for Discussion and Policy Purposes Only"))))
+                  
--- server.r
+++ server.r
@@ -16,42 +16,45 @@ function(input, output) {
   
   # Page 2, Overalls
   output$pg2_p1_line_all <- renderPlotly(plot_line_month_all)
-  output$pg2_d1_all <- renderDT(DT::datatable(po_all_top10_table, 
-                                          rownames = FALSE,
-                                          options = list(dom = "t",
-                                                         columnDefs = list(list(className = 'dt-left', targets = 0:1),
-                                                                           list(className = 'dt-right', targets = 2))),
-                                          caption = "Top 10 POs from all") %>% 
-                              formatCurrency(c("Sum Amount")))
+  
+  tbl1 <- DT::datatable(po_all_top10_table, 
+                        rownames = FALSE,
+                        options = list(dom = "t",
+                                       columnDefs = list(list(className = 'dt-left', targets = 0:1),
+                                                         list(className = 'dt-right', targets = 2))),
+                        caption = "Top 10 POs from all") %>% 
+    formatCurrency(c("Sum Amount"))
+  
+  output$pg2_d1_all <- renderDT(tbl1)
   
   output$pg2_p2_line_98pc <- renderPlotly(plot_line_month_98perc)
   output$pg2_d2_98pc <- renderDT(DT::datatable(po_98perc_top10_table, 
-                                           rownames = FALSE,
-                                           options = list(dom = "t",
-                                                          columnDefs = list(list(className = 'dt-left', targets = 0:1),
-                                                                            list(className = 'dt-right', targets = 2))),
-                                           caption = "Top 10 POs from 98% percentile.") %>% 
-                               formatCurrency(c("Sum Amount")))
+                                               rownames = FALSE,
+                                               options = list(dom = "t",
+                                                              columnDefs = list(list(className = 'dt-left', targets = 0:1),
+                                                                                list(className = 'dt-right', targets = 2))),
+                                               caption = "Top 10 POs from 98% percentile.") %>% 
+                                   formatCurrency(c("Sum Amount")))
   
   output$pg2_d3_prev2mo <- renderDT(DT::datatable(po_prev2mos_all_table,
-                                              rownames = FALSE, 
-                                              options = list(dom = "t")) %>% 
-                                  formatCurrency(c("Sum")) %>% 
-                                  formatStyle('Unit', 
-                                              target = 'row',
-                                              fontWeight = styleEqual(c("May Total", "Jun Total"), c('bold', 'bold')),
-                                              backgroundColor = styleEqual(c("May Total", "Jun Total"), c("#dedede", "#dedede"))))
+                                                  rownames = FALSE, 
+                                                  options = list(dom = "t")) %>% 
+                                      formatCurrency(c("Sum")) %>% 
+                                      formatStyle('Unit', 
+                                                  target = 'row',
+                                                  fontWeight = styleEqual(c("May Total", "Jun Total"), c('bold', 'bold')),
+                                                  backgroundColor = styleEqual(c("May Total", "Jun Total"), c("#dedede", "#dedede"))))
   
   output$pg2_p3_pie_spend_bunit <- renderPlotly(plot_pie_count_bunit)
   
   output$pg2_d4_ytd_bunit <- renderDT(DT::datatable(po_spend_bunit_all, 
-                                                rownames = FALSE, 
-                                                options = list(dom = "t")) %>%
-                                    formatCurrency(c("Sum")) %>% 
-                                    formatStyle('Unit',
-                                                target = 'row',
-                                                fontWeight = styleEqual(c("Grand Total"), c('bold')), 
-                                                backgroundColor = styleEqual(c("Grand Total"), c("#dedede"))))
+                                                    rownames = FALSE, 
+                                                    options = list(dom = "t")) %>%
+                                        formatCurrency(c("Sum")) %>% 
+                                        formatStyle('Unit',
+                                                    target = 'row',
+                                                    fontWeight = styleEqual(c("Grand Total"), c('bold')), 
+                                                    backgroundColor = styleEqual(c("Grand Total"), c("#dedede"))))
   
   # Pg4, Thresholds and Purchasing Platforms
   output$pg4_plot_thresholds <- renderPlotly(pg4_plot_thresholds)
@@ -60,9 +63,13 @@ function(input, output) {
                                                             rownames = FALSE, 
                                                             options = list(dom = 't')) %>% 
                                                 formatCurrency(c("FY18 Spend")) %>% 
-                                                formatPercentage(c("Percent of PO Count", "Percent of FY18 Spend"), digits = 0) %>% 
-                                                formatStyle("FY18 Thresholds", target = 'row', fontWeight = styleEqual(c("Grand Total"), c('bold')), 
-                                                            backgroundColor = styleEqual(c("Grand Total"), c("#dedede"))))
+                                                formatPercentage(c("Percent of PO Count", "Percent of FY18 Spend"), digits = 2) %>%
+                                                # this can be considered as a hack, since piping 2 formatStyle produces conflicts.
+                                                formatStyle("FY18 Thresholds", 
+                                                            target = 'row', 
+                                                            fontStyle = styleEqual(c("Total", ""), c('italic', 'normal')), 
+                                                            fontWeight = styleEqual(c("Total", ""), c('normal', 'bold')), 
+                                                            backgroundColor = styleEqual(c("Total", ""), c("#e6e6e6", "dedede"))))
   
   output$pg4_2moago_threshold <- renderDT(DT::datatable(pg4_2moago_threshold_all_table, 
                                                         colnames = paste(prevmos[1], colnames(pg4_2moago_threshold_all_table)),
@@ -83,5 +90,7 @@ function(input, output) {
                                             formatStyle("Threshold", 
                                                         target = 'row', fontWeight = styleEqual(c("Grand Total"), c('bold')), 
                                                         backgroundColor = styleEqual(c("Grand Total"), c("#dedede"))))
+  
+  output$pg4_plot_sub100_bunit_count <- renderPlotly(pg4_plot_sub1k_count_bunit)
   
 }
